33ea1a37700e3ef30268f46c34d30c3f49b05d6c
Molybdocene-dichloride <mineppist@yandex.ru>
2022-04-19 14:15:19 +0300

    slice method changes

--- src/native/library/headers/multiversion/client/graphics/Render.hpp
+++ src/native/library/headers/multiversion/client/graphics/Render.hpp
@@ -2,6 +2,8 @@
 
 #include <vector>
 #include <map>
+#include <string>
+#include <algorithm>
 
 #include <multiversion/client/graphics/Type.hpp>
 #include <multiversion/client/graphics/Operation.hpp>
@@ -17,33 +19,80 @@ namespace lagrangian {
 	namespace graphics {
         class LModel {
             public:
-            //std::vector<Magick::Image> textures;
+            virtual void operate(std::map<Indexes<long>, VertexOperation>& ops) = 0;
+            virtual void render(Tessellator tess) = 0;
+        };
+        class LConcreteModel : public LModel {
+            std::string id;
+
+            std::vector<Icon> icons;
             std::vector<Vertex> vertices;
 
-            std::vector<Vertex> slice(Range<long> range) {
-                return std::vector<Vertex>(vertices.begin() + range.min, vertices.begin() + range.max);
+            public:
+            std::vector<Operable> slice(Indexes<long>& range) {
+                if(range.type == OperableType::VERTEX) {
+                    std::vector<Vertex> vs{};
+                    std::vector<long>::iterator it;
+                    for(it = range.ts.begin(); it != range.ts.end(); it++) {
+                        vs.insert(vertices.at(range.ts.at(it)));
+                    }
+                    return vs;
+                } else if(range.type == OperableType::ICON) {
+                    std::vector<Icon> vs{};
+                    std::vector<long>::iterator it;
+                    for(it = range.ts.begin(); it != range.ts.end(); it++) {
+                        vs.insert(icons.at(range.ts.at(it)));
+                    }
+                    return vs;
+                }
+            }
+
+            virtual void operate(std::map<Indexes<long>, VertexOperation>& ops) {
+                std::map<Indexes<long>, VertexOperation>::iterator it;
+                for(it = ops.begin(); it != ops.end(); it++) {
+                    if(std::find(it->first.model_id.begin(), it->first.model_id.end(), id) != it->first.model_id.end()) it->second.apply(slice(it->first));
+                }
+            }
+            virtual void render(Tessellator tess) {
+                for(int e = 0; e < vertices.size(); e++) {
+                    tess.vertex(vertices.at(e).pos.x, vertices.at(e).pos.y, vertices.at(e).pos.z);
+                }
+            }
+        };
+        class LPackedModel : public LModel {
+            std::vector<LModel> models;
+
+            public:
+            virtual void operate(std::map<Indexes<long>, VertexOperation>& ops) {
+                for(int i = 0; i < count(); i++) {
+                    models.at(i).operate(ops);
+                }
+            }
+            virtual void render(Tessellator tess) {
+                for(int i = 0; i < count(); i++) {
+                    models.at(i).render(tess);
+                }
+            }
+
+            long count() {
+                return models.size();
             }
         };
         class LRenderPipeline {
             public:
-            std::map<Range<long>, VertexOperation> ops;
+            std::map<Indexes<long>, VertexOperation> ops;
 
             void operate(LModel& vs) {
-                std::map<Range<long>, VertexOperation>::iterator it;
-                for(it = ops.begin(); it != ops.end(); it++) {
-                    it->second.apply(vs.slice(it->first));
-                }
+                vs.operate(ops);
             }
         };
-        
+
         namespace LRenderer {
             Tessellator tess;
             void render(LRenderPipeline& p, LModel& vs) {
                 p.operate(vs);
 
-                for(int e = 0; e < vs.vertices.size(); e++) {
-                    tess.vertex(vs.vertices.at(e).pos.x, vs.vertices.at(e).pos.y, vs.vertices.at(e).pos.z);
-                }
+                vs.render(tess);
             }
         }
     }
--- src/native/library/headers/multiversion/client/graphics/Type.hpp
+++ src/native/library/headers/multiversion/client/graphics/Type.hpp
@@ -1,9 +1,17 @@
 #pragma once
 
+#include <vector>
+#include <string>
+
+enum class OperableType {
+    VERTEX,
+    ICON
+};
+
 template<class T> struct Vector3 {T x; T y; T z;};
 
 template<class T> struct Vector2 {T x; T y;};
 
-template<class T> struct Range {T min; T max;};
+template<class T> struct Indexes {OperableType type; std::vector<std::string> model_id; std::vector<T> ts;};
 
 struct Colour {unsigned char r; unsigned char g; unsigned char b; unsigned char a;};
